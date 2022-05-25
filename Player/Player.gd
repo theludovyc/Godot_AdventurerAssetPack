d76f653346c6d933bed8c232e0be2ca18161b877
Hugo Baba <Hugobarette@gmail.com>
2022-05-25 10:31:53 +0200

    Player's FSM using StateGraph

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,40 +4,37 @@ const Gravity = 14
 const Max_Speed = 320
 const Accel = 0.5
 const Deccel = 0.3
-const Jump_Acc = Gravity * 30
+const Jump_Acc = Gravity * 25
+
+const MIN_MOVE_VEL = 1.0
 
 var dir_x : int
 
 var velocity : Vector2
 
-var want_jump : bool
+signal jumped
 
-var default_state = "Idle"
-
-onready var state_machine = $StateMachine
-onready var animated_sprite = $AnimatedSprite
-
-func _process(_delta: float) -> void:
-	dir_x = int(Input.get_axis("ui_left", "ui_right"))
-
-	if Input.is_action_just_pressed("ui_accept"):
-		want_jump = true
-		state_machine.set_state("Jump")
-	elif want_jump:
-		want_jump = false
 
 func _physics_process(_delta: float) -> void:
-	if !is_on_floor():
-		velocity.y += Gravity
-	else:
-		velocity.y = Gravity
+	velocity.y += Gravity
 
 	if dir_x != 0:
 		velocity.x = lerp(velocity.x, sign(dir_x) * Max_Speed, Accel)
 	else:
 		velocity.x = lerp(velocity.x, 0, Deccel)
 
-	if want_jump:
-		velocity.y -= Jump_Acc
-
 	velocity = move_and_slide(velocity, Vector2.UP)
+
+
+
+func _input(_event: InputEvent) -> void:
+	dir_x = int(Input.get_axis("ui_left", "ui_right"))
+
+	if Input.is_action_just_pressed("ui_accept") && is_on_floor():
+		jump()
+
+
+
+func jump() -> void:
+	velocity.y = -Jump_Acc
+	emit_signal("jumped")
